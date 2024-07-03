888b09028282d511eef7f4ae9ef6172c1f238426
darkz77 <ngmhoang202@gmail.com>
2024-07-03 08:20:20 +0700

    udpate

--- app.py
+++ app.py
@@ -3,12 +3,15 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
+    if request.method == "POST":
+        name = request.form.get("name", "world")
+        return render_template("greet.html", name=name)
     return render_template("index.html")
 
 
-@app.route("/greet")
-def greet():
-    name = request.argv.get("name", "world")
-    return render_template(greet.html, name=name)
+# @app.route("/greet", methods=["POST"])
+# def greet():
+#     name = request.form.get("name", "world")
+#     return render_template("greet.html", name=name)
